3e6efc27c26ed2b51e15550cb637b482139f990d
MemonaKhan <you@example.comsuhail1975pk@yahoo.co.uk>
2020-07-23 00:34:54 +0500

    edited buttons

--- app.js
+++ app.js
@@ -3,45 +3,53 @@ var list = document.getElementById("list");
 function addTodo() {
     var todo_item = document.getElementById("to-do");
 
+    var item = document.createElement('div');
+
 
     // create input tag with text node
     var input = document.createElement('input');
     input.setAttribute("type", "text");
-    input.setAttribute("id", "itm-todo");
+    // input.setAttribute("id", "itm-todo");
+    // input.setAttribute("readonly","true");
+    // console.log("yes");
+
     var itmText = document.createTextNode(todo_item.value);
     input.appendChild(itmText);
 
     // create delete button
-    var delBtn = document.createElement("button")
-    var delText = document.createTextNode("DELETE")
-    delBtn.setAttribute("class", "dlt")
-    delBtn.setAttribute("onclick", "deleteItem(this)")
-    delBtn.appendChild(delText)
+    var delBtn = document.createElement("button");
+    var delText = document.createTextNode("DELETE");
+    delBtn.setAttribute("class", "dlt");
+    delBtn.setAttribute("onclick", "deleteItem(this)");
+    delBtn.appendChild(delText);
 
     // create edit button
     var editBtn = document.createElement("button");
-    var editText = document.createTextNode("EDIT")
-    editBtn.appendChild(editText)
-    editBtn.setAttribute("onclick", "editItem(this)")
+    var editText = document.createTextNode("EDIT");
+    editBtn.appendChild(editText);
+    editBtn.setAttribute("onclick", "editItem(this)");
 
+    list.appendChild(input);
+    list.appendChild(delBtn);
+    list.appendChild(editBtn);
 
-    li.appendChild(delBtn)
-    li.appendChild(editBtn)
+    
+    item.appendChild(list);
 
-    list.appendChild(li)
-
-    todo_item.value = ""
+    todo_item.value = "";
 }
 
 function deleteItem(e) {
-    e.parentNode.remove()
+    e.parentNode.parentNode.remove();
 }
 
 function editItem(e) {
-  var val = prompt("Enter updated value",e.parentNode.firstChild.nodeValue)
-  e.parentNode.firstChild.nodeValue = val;
+//   var val = prompt("Enter updated value",e.parentNode.firstChild.nodeValue)
+//   e.parentNode.firstChild.nodeValue = val;
+input.setAttribute("readonly","false");
+
 }
 
-function deleteAll() {
-    list.innerHTML = ""
-}
+// function deleteAll() {
+//     list.innerHTML = ""
+// }
